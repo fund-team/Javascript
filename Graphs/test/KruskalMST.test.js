a4a2fad80b246c90165e2c2ce6c27155061aab73
Eyskha <chloe.porion@gmail.com>
2022-03-03 17:18:11 +0100

    feat: Refactor KruskalMST File in "Graphs" directory. Add the corresponding test file. New class "WeightedUndirectedGraph" in the "Data-Structures" directory to factorize: definition and test files. Import "UnionFind" function from existing file in "Search" directory.
    Part of issue #3

---
+++ Graphs/test/KruskalMST.test.js
@@ -0,0 +1,24 @@
+import { WeightedUndirectedGraph } from "../../Data-Structures/Graph/WeightedUndirectedGraph"
+import { kruskalMST } from '../KruskalMST'
+
+describe('Test kruskalMST function', () => {
+  const graph = new WeightedUndirectedGraph()
+
+  // adding weighted edges
+  graph.addEdge(1, 2, 1)
+  graph.addEdge(1, 3, 1)
+  graph.addEdge(2, 3, 2)
+  graph.addEdge(3, 4, 1)
+  graph.addEdge(3, 5, 100)
+  graph.addEdge(4, 5, 5)
+
+  it('Should delete the edge with weight 100 and the one between 2 and 3', () => {
+    const mstGraph = kruskalMST(graph)
+    expect(mstGraph.adjacencyMap).toEqual({
+      1: {2: 1, 3: 1},
+      2: {1: 1},
+      3: {4: 1, 1: 1},
+      4: {3: 1, 5: 5},
+      5: {4: 5}})
+  })
+})
