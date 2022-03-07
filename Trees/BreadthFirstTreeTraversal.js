2cccea89069073ccd5dc410333b343e359d61ec6
Nicky Obreykov <nicky.obreykov@gmail.com>
2022-03-07 01:26:01 +0100

    feat: Refractor Breadth First Tree Traversal
    Refractoring to use the Queue data structure present in the
    "Data-Structures" directory. Added generic and binary tree data
    structure and corresponding tests.
    
    Part of issue #18

--- Trees/BreadthFirstTreeTraversal.js
+++ Trees/BreadthFirstTreeTraversal.js
@@ -1,51 +1,53 @@
 /*
-  Breadth First Tree Traversal or level order traversal implementation in javascript
+  Breadth First Tree Traversal or level order traversal implementation using Queue datastructure in javascript
   Author: @GerardUbuntu
 */
 
-class Node {
-  constructor (data) {
-    this.data = data
-    this.left = null
-    this.right = null
+import { Queue } from '../Data-Structures/Queue/Queue'
+
+function breadthFirstTreeTraversal(tree) {
+  const queue = new Queue()
+  const result = []
+
+  if (typeof tree !== 'undefined' && typeof tree.root !== 'undefined') {
+    queue.enqueue(tree.root)
   }
+
+  while (!queue.empty()) {
+    const node = queue.dequeue()
+    result.push(node.value)
+
+    if (node.children.length !== 0) {
+      node.children.forEach((element) => {
+        queue.enqueue(element)
+      })
+    }
+  }
+
+  return result
 }
 
-class BinaryTree {
-  constructor () {
-    this.root = null
-    this.traversal = []
+function breadthFirstBinaryTreeTraversal(tree) {
+  const queue = new Queue()
+  const result = []
+
+  if (typeof tree !== 'undefined' && typeof tree.root !== 'undefined') {
+    queue.enqueue(tree.root)
   }
 
-  breadthFirst () {
-    const h = this.getHeight(this.root)
-    for (let i = 0; i !== h; i++) {
-      this.traverseLevel(this.root, i)
+  while (!queue.empty()) {
+    const node = queue.dequeue()
+    result.push(node.value)
+
+    if (node.left !== null) {
+      queue.enqueue(node.left)
     }
-    return this.traversal
+    if (node.right !== null) {
+      queue.enqueue(node.right)
+    }
   }
 
-  // Computing the height of the tree
-  getHeight (node) {
-    if (node === null) {
-      return 0
-    }
-    const lheight = this.getHeight(node.left)
-    const rheight = this.getHeight(node.right)
-    return lheight > rheight ? lheight + 1 : rheight + 1
-  }
-
-  traverseLevel (node, levelRemaining) {
-    if (node === null) {
-      return
-    }
-    if (levelRemaining === 0) {
-      this.traversal.push(node.data)
-    } else {
-      this.traverseLevel(node.left, levelRemaining - 1)
-      this.traverseLevel(node.right, levelRemaining - 1)
-    }
-  }
+  return result
 }
 
-export { BinaryTree, Node }
+export { breadthFirstTreeTraversal, breadthFirstBinaryTreeTraversal }
--- Trees/test/BreadthFirstTreeTraversal.test.js
+++ Trees/test/BreadthFirstTreeTraversal.test.js
@@ -1,15 +1,27 @@
-import { BinaryTree, Node } from '../BreadthFirstTreeTraversal'
+import {
+  breadthFirstTreeTraversal,
+  breadthFirstBinaryTreeTraversal
+} from '../BreadthFirstTreeTraversal'
+import {
+  Tree,
+  GenericTreeNode,
+  BinaryTreeNode
+} from '../../Data-Structures/Tree/Tree'
 
-describe('Breadth First Tree Traversal', () => {
-  const binaryTree = new BinaryTree()
+describe('Breadth First Binary Tree Traversal', () => {
+  const rootNode = new BinaryTreeNode(7)
+  const n1 = new BinaryTreeNode(5)
+  const n2 = new BinaryTreeNode(8)
+  const n3 = new BinaryTreeNode(3)
+  const n4 = new BinaryTreeNode(6)
+  const n5 = new BinaryTreeNode(9)
 
-  const root = new Node(7)
-  root.left = new Node(5)
-  root.right = new Node(8)
-  root.left.left = new Node(3)
-  root.left.right = new Node(6)
-  root.right.right = new Node(9)
-  binaryTree.root = root
+  rootNode.left = n1
+  rootNode.right = n2
+  n1.left = n3
+  n1.right = n4
+  n2.right = n5
+  const tree = new Tree(rootNode)
 
   // Visualization :
   //
@@ -20,8 +32,40 @@ describe('Breadth First Tree Traversal', () => {
   //        3   6   9
 
   it('Binary tree - Level order traversal', () => {
-    expect(binaryTree.traversal).toStrictEqual([])
-    const traversal = binaryTree.breadthFirst()
+    expect(breadthFirstBinaryTreeTraversal()).toStrictEqual([])
+    const traversal = breadthFirstBinaryTreeTraversal(tree)
     expect(traversal).toStrictEqual([7, 5, 8, 3, 6, 9])
+  })
+})
+
+describe('Breadth First Generic Tree Traversal', () => {
+  const rootNode = new GenericTreeNode(7)
+  const n1 = new GenericTreeNode(5)
+  const n2 = new GenericTreeNode(8)
+  const n3 = new GenericTreeNode(3)
+  const n4 = new GenericTreeNode(4)
+  const n5 = new GenericTreeNode(6)
+  const n6 = new GenericTreeNode(9)
+
+  rootNode.addChild(n1)
+  rootNode.addChild(n2)
+  n1.addChild(n3)
+  n1.addChild(n4)
+  n1.addChild(n5)
+  n2.addChild(n6)
+  const tree = new Tree(rootNode)
+
+  // Visualization :
+  //
+  //            7
+  //           / \
+  //          5   8
+  //         /|\   \
+  //        3 4 6   9
+
+  it('Binary tree - Level order traversal', () => {
+    expect(breadthFirstTreeTraversal()).toStrictEqual([])
+    const traversal = breadthFirstTreeTraversal(tree)
+    expect(traversal).toStrictEqual([7, 5, 8, 3, 4, 6, 9])
   })
 })
